7462f29089ec9c44a6af6464e00fc5c0578519ab
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-09-02 16:55:15 +0300

    fix the local mistake and display the logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
 import Header from './components/Header/Header'
+import Logo from './components/Logo/Logo'
 function App() {
   return (
     <> 
     <div className="test">
+    <Logo/>
     <Header/>
     </div>
     </>
